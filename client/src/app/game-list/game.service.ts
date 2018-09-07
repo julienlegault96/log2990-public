fff5f19026f489890187aa19e719c46307dc101c
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-07 16:59:40 -0400

    Added game service with dummy games

---
+++ client/src/app/game-list/game.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+import { Game } from '../../../../common/game/game';
+import { Leaderboard } from '../../../../common/game/leaderboard';
+
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+// import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GameService {
+
+    private gamesUrl = 'api/games';  // URL to web api
+
+    constructor(private http: HttpClient) { }
+
+    getGames(): Observable<Game[]> {
+        let game = new Game();
+        game.title = "test";
+        game.imageUrl = "url123.txt";
+
+        let tempLeaderboard = new Leaderboard();
+        tempLeaderboard.title = "solo";
+        tempLeaderboard.times = [12, 46, 36];
+        game.leaderboards = new Array();
+        game.leaderboards.push(tempLeaderboard);
+
+        tempLeaderboard = new Leaderboard();
+        tempLeaderboard.title = "1v1";
+        tempLeaderboard.times = [35, 57, 92];
+        game.leaderboards.push(tempLeaderboard);
+
+        let games = new Array(game);
+
+        game.title = "beaioj";
+
+        games.push(game);
+        return of(games);
+        // return this.http.get<Game[]>(this.gamesUrl)
+        //     .pipe(
+        //         tap(games => this.log('fetched games')),
+        //         catchError(this.handleError('getGames', []))
+        //     );
+    }
+}
--- client/src/app/game-list/games/games.component.ts
+++ client/src/app/game-list/games/games.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GameService } from '../game.service';
 import { Game } from '../../../../../common/game/game';
-import { Leaderboard } from '../../../../../common/game/leaderboard';
 
 @Component({
     selector: 'app-games',
@@ -12,34 +12,15 @@ export class GamesComponent implements OnInit {
 
     public games: Game[];
 
-    constructor() { }
+    constructor(private gameService: GameService) { }
 
     ngOnInit() {
         this.getGames();
     }
 
-    getGames() {
-
-        let game = new Game();
-        game.title = "test";
-        game.imageUrl = "url123.txt";
-
-        let tempLeaderboard = new Leaderboard();
-        tempLeaderboard.title = "solo";
-        tempLeaderboard.times = [12,46,36];
-        game.leaderboards = new Array();
-        game.leaderboards.push(tempLeaderboard);
-        
-        tempLeaderboard = new Leaderboard();
-        tempLeaderboard.title = "1v1";
-        tempLeaderboard.times = [35,57,92];
-        game.leaderboards.push(tempLeaderboard);
-
-        this.games = new Array(game);
-
-        game.title = "beaioj";
-
-        this.games.push(game);
+    getGames(): void {
+        this.gameService.getGames()
+            .subscribe(games => this.games = games);
     }
 
 }
