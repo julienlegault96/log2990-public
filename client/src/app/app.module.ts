dc77662bae862784c34f1e4d96c0523589f38436
Sophie Bosse <sobos@gigl.polymtl.ca>
2018-09-07 16:31:15 -0400

    Ajout d'un component pour le chronometre, ND, SB

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BasicService } from "./basic.service";
 import { HttpClientModule } from "@angular/common/http";
 import { JeuSoloComponent } from './jeu-solo/jeu-solo.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { ChronometreComponent } from './chronometre/chronometre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JeuSoloComponent
+    JeuSoloComponent,
+    ChronometreComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/jeu-solo/jeu-solo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jeu-solo',
+  templateUrl: './jeu-solo.component.html',
+  styleUrls: ['./jeu-solo.component.css']
+})
+export class JeuSoloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
