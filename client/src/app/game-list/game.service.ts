f57e89045676e89f5ed3b0ec8807ca983b0be9ac
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-09 18:58:02 -0400

    Rearranged dummy data for single and double view games

--- client/src/app/game-list/game.service.ts
+++ client/src/app/game-list/game.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
-
-import { Game } from '../../../../common/game/game';
-import { Leaderboard } from '../../../../common/game/leaderboard';
-
 import { Observable, of } from 'rxjs';
 // import { HttpClient } from '@angular/common/http';
 // import { catchError, map, tap } from 'rxjs/operators';
 
+import { Game } from '../../../../common/game/game';
+
 @Injectable({
     providedIn: 'root'
 })
+
 export class GameService {
 
     // private gamesUrl = 'api/games';  // URL to web api
@@ -18,55 +17,81 @@ export class GameService {
     constructor() { }
 
     getSingleViewGames(): Observable<Game[]> {
-        let game = new Game();
-        game.title = "test";
-        game.imageUrl = "url123.txt";
-
-        let tempLeaderboard = new Leaderboard();
-        tempLeaderboard.title = "solo";
-        tempLeaderboard.times = [12, 46, 36];
-        game.leaderboards = new Array();
-        game.leaderboards.push(tempLeaderboard);
-
-        tempLeaderboard = new Leaderboard();
-        tempLeaderboard.title = "1v1";
-        tempLeaderboard.times = [35, 57, 92];
-        game.leaderboards.push(tempLeaderboard);
-
-        let games = new Array(game);
-
-        game.title = "beaioj";
-
-        games.push(game);
-        return of(games);
         // return this.http.get<Game[]>(this.gamesUrl)
         //     .pipe(
         //         tap(games => this.log('fetched games')),
         //         catchError(this.handleError('getGames', []))
         //     );
+        return of(dummySingleViewGames);
     }
 
     getDoubleViewGames(): Observable<Game[]> {
-        let game = new Game();
-        game.title = "test";
-        game.imageUrl = "url123.txt";
-
-        let tempLeaderboard = new Leaderboard();
-        tempLeaderboard.title = "solo";
-        tempLeaderboard.times = [12, 46, 36];
-        game.leaderboards = new Array();
-        game.leaderboards.push(tempLeaderboard);
-
-        tempLeaderboard = new Leaderboard();
-        tempLeaderboard.title = "1v1";
-        tempLeaderboard.times = [35, 57, 92];
-        game.leaderboards.push(tempLeaderboard);
-
-        let games = new Array(game);
-
-        game.title = "beaioj";
-
-        games.push(game);
-        return of(games);
+        return of(dummyDoubleViewGames);
     }
 }
+
+const dummySingleViewGames = [
+    {
+        id: 1,
+        title: 'SingleViewGame 1',
+        imageUrl: 'singleViewGameImage1.bmp',
+        leaderboards: [
+            {
+                title: 'Solo',
+                times: [65, 72, 83]
+            },
+            {
+                title: 'One versus One',
+                times: [45, 51, 58]
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'SingleViewGame 2',
+        imageUrl: 'singleViewGameImage2.bmp',
+        leaderboards: [
+            {
+                title: 'Solo',
+                times: [46, 53, 59]
+            },
+            {
+                title: 'One versus One',
+                times: [35, 39, 41]
+            }
+        ]
+    }
+];
+
+const dummyDoubleViewGames = [
+    {
+        id: 3,
+        title: 'DoubleViewGame 1',
+        imageUrl: 'doubleviewgameimage1.bmp',
+        leaderboards: [
+            {
+                title: 'Solo',
+                times: [65, 72, 83]
+            },
+            {
+                title: 'One versus One',
+                times: [45, 51, 58]
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: 'DoubleViewGame 2',
+        imageUrl: 'doubleviewgameimage2.bmp',
+        leaderboards: [
+            {
+                title: 'Solo',
+                times: [46, 53, 59]
+            },
+            {
+                title: 'One versus One',
+                times: [35, 39, 41]
+            }
+        ]
+    }
+];
