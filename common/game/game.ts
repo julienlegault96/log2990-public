2568a77aaab282089e67e328ba164ffa8c3274ce
Gabriel Bottari Defrance <gabriel.bottari.defrance@gmail.com>
2018-09-14 14:27:32 -0400

    Modification du model

--- common/game/game.ts
+++ common/game/game.ts
@@ -1,7 +1,13 @@
 import { Leaderboard } from './leaderboard';
 
+enum GameType {
+    SingleView = 0,
+    DoubleView = 1
+}
+
 export class Game {
+    type: GameType;
     title: string;
-    imageUrl: string;
+    imageUrl: string[];
     leaderboards: Leaderboard[];
 }
