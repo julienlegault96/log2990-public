4f34b9c76db966b528e9c6439f74f7c7cf5ee54e
JulienLegault96 <julien-3.legault@polymtl.ca>
2018-09-14 16:54:21 -0400

    traduction dun commentaire

--- client/src/app/User/user.service.ts
+++ client/src/app/User/user.service.ts
@@ -9,7 +9,7 @@ import { USERS } from "./mock-users";
 export class UserService {
     private static minUsername: number = 1;
     private static maxUsername: number = 20;
-    // rom https://stackoverflow.com/a/389022
+    // provient de https://stackoverflow.com/a/389022
     private static validationRegEx: RegExp = /^[a-z0-9]+$/i;
 
     private constructor() { }
