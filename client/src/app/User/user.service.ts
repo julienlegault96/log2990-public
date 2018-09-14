dc64f6a02a0d0127bb017a915e3b63b6c63ee246
julienlegault96 <julien-3.legault@polymtl.ca>
2018-09-14 11:16:54 -0400

    modification des variables min et max pour le nom d usager

--- client/src/app/User/user.service.ts
+++ client/src/app/User/user.service.ts
@@ -7,8 +7,8 @@ import { USERS } from "./mock-users";
     providedIn: "root"
 })
 export class UserService {
-    private static minUsername: number = 3;
-    private static maxUsername: number = 6;
+    private static minUsername: number = 1;
+    private static maxUsername: number = 20;
     private static validationRegEx: RegExp = /([A-Za-z0-9]){1,}/;
     private constructor() { }
 
