7715834d161250c9c80ac21211789be5adee5308
Louis Sérey Larose <papalarose@yahoo.ca>
2018-09-19 12:37:14 -0400

    ajout de users dans le router du serveur
    LSLarose & JL

--- server/app/routes.ts
+++ server/app/routes.ts
@@ -3,19 +3,38 @@ import { Router, Request, Response, NextFunction } from "express";
 
 import Types from "./types";
 import { Games } from "./routes/games";
+import { Users } from "./routes/users";
 
 @injectable()
 export class Routes {
 
-    public constructor(@inject(Types.Games) private games: Games) { }
+    public constructor(
+        @inject(Types.Games) private games: Games,
+        @inject(Types.Users) private users: Users
+        ) { }
 
     public get routes(): Router {
         const router: Router = Router();
 
-        router.get("/games",
-            (req: Request, res: Response, next: NextFunction) => this.games.getGames(req, res, next));
-        router.post("/games",
-            (req: Request, res: Response, next: NextFunction) => this.games.addGame(req, res, next));
+        // GAMES
+        router.get(
+            "/games",
+            (req: Request, res: Response, next: NextFunction) => this.games.getGames(req, res, next)
+            );
+        router.post(
+            "/games",
+            (req: Request, res: Response, next: NextFunction) => this.games.addGame(req, res, next)
+            );
+
+        // USERS
+        router.get(
+            "/users",
+            (req: Request, res: Response, next: NextFunction) => this.users.getUsers(req, res, next)
+            );
+        router.post(
+            "/users",
+            (req: Request, res: Response, next: NextFunction) => this.users.addUser(req, res, next)
+            );
 
         return router;
     }
