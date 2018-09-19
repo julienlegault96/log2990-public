d4f487ca876f5d5bf7a6ef6a862f274e24f35fa5
julienlegault96 <julien-3.legault@polymtl.ca>
2018-09-19 14:17:31 -0400

    creation du test pour verifier la precense des noms dutilisateurs JL & LSLarose

--- client/src/app/User/user-component/user.component.spec.ts
+++ client/src/app/User/user-component/user.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientModule } from "@angular/common/http";
 
 import { UserComponent } from "./user.component";
 import { FormsModule } from "@angular/forms";
@@ -10,7 +11,7 @@ describe("UserComponent", () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [UserComponent],
-            imports: [FormsModule]
+            imports: [FormsModule, HttpClientModule]
         })
             .compileComponents();
     }));
