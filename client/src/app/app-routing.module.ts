dcefd6b53f499be8327b41be0b43b87b7f84dfdc
unknown <sophie.bosse2@gmail.com>
2018-09-19 19:07:00 -0400

    Ajout du routing entre les pages index, listeJeux et soloGame. SB

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { SoloGameComponent } from "./game/solo-game/solo-game.component";
+import {UserComponent} from "./User/user-component/user.component";
+import {GameListComponent} from "./game-list-menu/game-list/game-list.component";
 
 const routes: Routes = [
-  { path: "soloGame", component: SoloGameComponent }
+  { path: "soloGame", component: SoloGameComponent},
+  { path: "index", component: UserComponent},
+  { path: "listeJeux", component: GameListComponent},
+  { path: "", redirectTo: "/index", pathMatch: "full" },
 ];
 
 @NgModule({
--- client/src/app/game-list-menu/game-card/game-card.component.ts
+++ client/src/app/game-list-menu/game-card/game-card.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
-import { GameService } from '../game.service';
+import { GameService } from "../game.service";
 
-import { Game } from '../../../../../common/game/game';
+import { Game } from "../../../../../common/game/game";
 
 @Component({
-    selector: 'app-game',
-    templateUrl: './game-card.component.html',
-    styleUrls: ['./game-card.component.css']
+    selector: "app-game",
+    templateUrl: "./game-card.component.html",
+    styleUrls: ["./game-card.component.css"]
 })
 export class GameCardComponent implements OnInit {
 
