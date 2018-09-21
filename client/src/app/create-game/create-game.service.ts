f5f4ba990111bc71289d219cac54288e79dbddf4
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-21 15:20:47 -0400

    Verification des inputs

--- client/src/app/create-game/create-game.service.ts
+++ client/src/app/create-game/create-game.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CreateGameService {
 
-  constructor() { }
+    private readonly nameMinLength: number = 1;
+    private readonly nameMaxLength: number = 20;
+    private readonly imageFileExtension: string = ".bmp";
+
+    constructor() { }
+
+    public submit(name: String, images: File[]): void {
+        if (!this.isValidInputList(name, images)) {
+            return;
+        }
+        alert("Submited!");
+    }
+
+    public isValidInputList(name: String, images: File[]): boolean {
+        return this.isValidName(name)
+            && this.isValidInputImageList(images);
+    }
+
+    public isValidName(name: String): boolean {
+        return name
+            && name.length >= this.nameMinLength
+            && name.length <= this.nameMaxLength;
+    }
+
+    public getNameMaxLength(): number {
+        return this.nameMaxLength;
+    }
+
+    private isValidInputImageList(images: File[]): boolean {
+        for (let image of images) {
+            if (!this.isValidImage(image)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private isValidImage(image: File): boolean {
+        return image != null
+            && String(image.name).indexOf(this.imageFileExtension) != -1;
+    }
 }
