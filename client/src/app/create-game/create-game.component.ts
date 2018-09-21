ca2a4540ad002040f4cd7c62ae1e4c262c49f742
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-21 11:30:04 -0400

    Premier template de l'ajout de jeu

--- client/src/app/create-game/create-game.component.ts
+++ client/src/app/create-game/create-game.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-create-game',
-  templateUrl: './create-game.component.html',
-  styleUrls: ['./create-game.component.css']
+    selector: 'app-create-game',
+    templateUrl: './create-game.component.html',
+    styleUrls: ['./create-game.component.css']
 })
 export class CreateGameComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
