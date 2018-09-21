a4185aa3f98e257597ee25569ccda9d1bb4e4eb2
Louis Sérey Larose <papalarose@yahoo.ca>
2018-09-21 10:03:29 -0400

    application des tab = 4spc
    LSLarose

--- client/src/app/game-list-menu/leaderboard/leaderboard.component.spec.ts
+++ client/src/app/game-list-menu/leaderboard/leaderboard.component.spec.ts
@@ -4,29 +4,29 @@ import { LeaderboardComponent } from "./leaderboard.component";
 import { Leaderboard } from "../../../../../common/game/leaderboard";
 
 describe("LeaderboardComponent", () => {
-  let component: LeaderboardComponent;
-  let fixture: ComponentFixture<LeaderboardComponent>;
-  const mockLeaderboard: Leaderboard = {title: "test-Leaderboard", scores: []};
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LeaderboardComponent ],
-      providers: [{ provide: Leaderboard, useValue: mockLeaderboard }]
-    })
-    .compileComponents();
-  }));
+    let component: LeaderboardComponent;
+    let fixture: ComponentFixture<LeaderboardComponent>;
+    const mockLeaderboard: Leaderboard = { title: "test-Leaderboard", scores: [] };
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [LeaderboardComponent],
+            providers: [{ provide: Leaderboard, useValue: mockLeaderboard }]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LeaderboardComponent);
-    component = fixture.componentInstance;
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LeaderboardComponent);
+        component = fixture.componentInstance;
+    });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 
-  it("leaderboard should change on input", () => {
-    component.leaderboard = mockLeaderboard;
-    fixture.detectChanges();
-    expect(fixture.nativeElement.querySelector("h5").innerText).toEqual(mockLeaderboard.title);
-  });
+    it("leaderboard should change on input", () => {
+        component.leaderboard = mockLeaderboard;
+        fixture.detectChanges();
+        expect(fixture.nativeElement.querySelector("h5").innerText).toEqual(mockLeaderboard.title);
+    });
 });
