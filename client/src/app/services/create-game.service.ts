ab7499acf7372dac0cea53ca51248922f6fe7ae3
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-22 17:20:08 -0400

    Ajout de tests

--- client/src/app/services/create-game.service.ts
+++ client/src/app/services/create-game.service.ts
@@ -15,29 +15,15 @@ export class CreateGameService extends GameService {
         if (!this.isValidInputList(name, images)) {
             return;
         }
+
         const rawImagePromise: Promise<string> = this.getBase64(images[0]);
         const modifiedImagePromise: Promise<string> = this.getBase64(images[1]);
 
-        Promise.all([rawImagePromise, modifiedImagePromise]).then((values) => {
-            const newGame: Game = newGameTemplate;
-            newGame.type = GameType.SingleView;
-            newGame.title = name;
-            newGame.imageUrl = [
-                values[0],
-                values[1]
-            ];
+        Promise.all([rawImagePromise, modifiedImagePromise]).then((imageUrls) => {
+            const newGame: Game = this.generateGame(name, imageUrls);
             this.addGame(newGame).subscribe((game: Game) => {
                 alert("Uploaded");
             });
-        });
-    }
-
-    private getBase64(file: File): Promise<string> {
-        return new Promise((resolve, reject) => {
-            const reader: FileReader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = () => resolve(String(reader.result));
-            reader.onerror = (error) => reject(error);
         });
     }
 
@@ -47,13 +33,34 @@ export class CreateGameService extends GameService {
     }
 
     public isValidName(name: String): boolean {
-        return name
+        return Boolean(name)
             && name.length >= this.nameMinLength
             && name.length <= this.nameMaxLength;
     }
 
     public getNameMaxLength(): number {
         return this.nameMaxLength;
+    }
+
+    private generateGame(name: string, imageUrls: Array<string>): Game {
+        const newGame: Game = newGameTemplate;
+        newGame.type = GameType.SingleView;
+        newGame.title = name;
+        newGame.imageUrl = [
+            imageUrls[0],
+            imageUrls[1]
+        ];
+
+        return newGame;
+    }
+
+    private getBase64(file: File): Promise<string> {
+        return new Promise((resolve, reject) => {
+            const reader: FileReader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(String(reader.result));
+            reader.onerror = (error) => reject(error);
+        });
     }
 
     private isValidInputImageList(images: File[]): boolean {
