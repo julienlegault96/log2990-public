68a1337bce78622a740ab27251143239c17b8206
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-22 16:38:37 -0400

    Ajout test createGameComponent

--- common/game/game.ts
+++ common/game/game.ts
@@ -7,3 +7,45 @@ export class Game {
     imageUrl: string[];
     leaderboards: Leaderboard[];
 }
+
+export const newGameTemplate: Game = {
+    "type": GameType.SingleView,
+    "title": "Nouveau jeu",
+    "imageUrl": ["", ""],
+    "leaderboards": [
+        {
+            "title": "Solo",
+            "scores": [
+                {
+                    "username": "Username1",
+                    "time": 34
+                },
+                {
+                    "username": "Username2",
+                    "time": 46
+                },
+                {
+                    "username": "Username3",
+                    "time": 67
+                },
+            ]
+        },
+        {
+            "title": "1 vs 1",
+            "scores": [
+                {
+                    "username": "Username11",
+                    "time": 15
+                },
+                {
+                    "username": "Username22",
+                    "time": 25
+                },
+                {
+                    "username": "Username33",
+                    "time": 35
+                },
+            ]
+        }
+    ]
+};
