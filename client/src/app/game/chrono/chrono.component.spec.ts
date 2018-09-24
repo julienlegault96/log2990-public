5ed813e50b3151ee68cdef4632d2c69362a40ca4
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-24 15:35:57 -0400

    Format

--- client/src/app/game/chrono/chrono.component.spec.ts
+++ client/src/app/game/chrono/chrono.component.spec.ts
@@ -23,13 +23,11 @@ describe("ChronoComponent", () => {
         expect(component).toBeTruthy();
     });
 
-    it("should start at 0",
-       () => {
-            expect(component.time).toBe(0);
-        });
+    it("should start at 0", () => {
+        expect(component.time).toBe(0);
+    });
 
-    it("should write 00:00 at start",
-       () => {
-            expect(component.printedTime).toBe("00:00");
-        });
+    it("should write 00:00 at start", () => {
+        expect(component.printedTime).toBe("00:00");
+    });
 });
