569d755137e3203300dd13c175fa67dbda0b21c9
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-28 11:35:44 -0400

    Extraction classes. KP

--- client/src/app/game/game-list-menu/game-card/game-card.component.ts
+++ client/src/app/game/game-list-menu/game-card/game-card.component.ts
@@ -1,22 +1,7 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component } from "@angular/core";
 
 import { GameService } from "../../../services/game.service";
-
-import { Game } from "../../../../../../common/game/game";
-
-export abstract class AbstractGameCardComponent implements OnInit {
-
-    @Input() public game: Game;
-    public isJoinable: boolean;
-
-    public constructor(protected gameService: GameService) { }
-
-    public ngOnInit(): void {
-        this.gameService.isJoinable(this.game)
-            .subscribe((isJoinable) => this.isJoinable = isJoinable);
-    }
-
-}
+import { AbstractGameCardComponent } from "src/app/game/abstract-game-card/abstract-game-card.component";
 
 @Component({
     selector: "app-game",
