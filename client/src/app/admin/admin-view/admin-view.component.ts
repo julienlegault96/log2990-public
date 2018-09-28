f5148c38b55f20209cf5889157f3b55fecd952b1
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-28 11:25:08 -0400

    Extraction classe

--- client/src/app/admin/admin-view/admin-view.component.ts
+++ client/src/app/admin/admin-view/admin-view.component.ts
@@ -1,7 +1,6 @@
 import { Component } from "@angular/core";
 import { AbsGameListComponent } from "../../game/game-list-menu/game-list/game-list.component";
 import { GameService } from "../../services/game.service";
-import { AbsGameCardComponent } from "../../game/game-list-menu/game-card/game-card.component";
 
 @Component({
     selector: "app-admin-view",
@@ -13,28 +12,6 @@ export class AdminViewComponent extends AbsGameListComponent {
 
     public constructor(protected gameService: GameService) {
         super(gameService);
-    }
-
-}
-
-@Component({
-    selector: "app-admin-card",
-    templateUrl: "./admin-view-card.component.html",
-    styleUrls: ["../../game/game-list-menu/game-card/game-card.component.css"]
-})
-export class AdminViewCardComponent extends AbsGameCardComponent {
-
-    public constructor(gameService: GameService) {
-        super(gameService);
-    }
-
-    public resetLeaderboard(): void {
-        this.gameService.resetLeaderboard(this.game).subscribe();
-    }
-
-    public delete(): void {
-        this.gameService.deleteGame(this.game).subscribe();
-        alert("Veuillez actualiser le navigateur pour voir le changement!");
     }
 
 }
