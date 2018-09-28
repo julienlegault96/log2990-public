678c9a33a3863c5293723fb432a5e1b6fde22384
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-28 11:40:51 -0400

    Remove css file. KP

--- client/src/app/game/game-list-menu/game-card/game-card.component.ts
+++ client/src/app/game/game-list-menu/game-card/game-card.component.ts
@@ -1,13 +1,13 @@
 import { Component } from "@angular/core";
 
-import { GameService } from "../../../services/game.service";
+import { GameService } from "src/app/services/game.service";
 import { AbstractGameCardComponent } from "src/app/game/abstract-game-card/abstract-game-card.component";
 
 @Component({
     selector: "app-game",
     templateUrl: "./game-card.component.html",
-    styleUrls: ["./game-card.component.css"]
 })
+
 export class GameCardComponent extends AbstractGameCardComponent {
 
     public constructor(gameService: GameService) {
