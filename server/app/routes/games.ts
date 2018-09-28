82aa3a06fe50946a93005b0ebb63fd599490f10e
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-28 16:24:41 -0400

    Formatting. KP

--- server/app/routes/games.ts
+++ server/app/routes/games.ts
@@ -2,9 +2,10 @@ import { Request, Response, NextFunction } from "express";
 import { inject, injectable } from "inversify";
 
 import Types from "../types";
-import { Game } from "../../../common/game/game";
 import { Mongo, Collections } from "../services/mongo";
 import { AbstractRoute } from "./abstract-route/abstract-route";
+
+import { Game } from "../../../common/game/game";
 
 @injectable()
 
--- server/app/routes/users.ts
+++ server/app/routes/users.ts
@@ -1,9 +1,10 @@
 import { inject, injectable } from "inversify";
 
 import Types from "../types";
-import { User } from "../../../common/user/user";
 import { Mongo, Collections } from "../services/mongo";
 import { AbstractRoute } from "./abstract-route/abstract-route";
+
+import { User } from "../../../common/user/user";
 
 @injectable()
 export class Users extends AbstractRoute<User> {
