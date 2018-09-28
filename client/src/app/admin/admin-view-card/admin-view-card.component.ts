90a20b7bd4ef3f3beff0cb988921bc6d65efa6a0
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-28 11:31:13 -0400

    Renommer classe abstraite. KP

--- client/src/app/admin/admin-view-card/admin-view-card.component.ts
+++ client/src/app/admin/admin-view-card/admin-view-card.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { GameService } from "../../services/game.service";
-import { AbsGameCardComponent } from "../../game/game-list-menu/game-card/game-card.component";
+import { AbstractGameCardComponent } from "../../game/game-list-menu/game-card/game-card.component";
 
 @Component({
     selector: "app-admin-card",
@@ -8,7 +8,7 @@ import { AbsGameCardComponent } from "../../game/game-list-menu/game-card/game-c
     styleUrls: ["../../game/game-list-menu/game-card/game-card.component.css"]
 })
 
-export class AdminViewCardComponent extends AbsGameCardComponent {
+export class AdminViewCardComponent extends AbstractGameCardComponent {
 
     public constructor(gameService: GameService) {
         super(gameService);
