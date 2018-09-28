891c7aec243964194de64a6b8b04be86f48750c3
Gabriel Bottari <g.bottari@chocolat.chaud.io>
2018-09-27 22:28:01 -0400

    Fix linting for game.ts routes

--- server/app/routes/games.ts
+++ server/app/routes/games.ts
@@ -2,9 +2,9 @@ import { Request, Response, NextFunction } from "express";
 import { inject, injectable } from "inversify";
 import Types from "../types";
 import { Game } from "../../../common/game/game";
-import { Mongo, Collections} from "../services/mongo";
+import { Mongo, Collections } from "../services/mongo";
 import { CODES } from "../../../common/communication/response-codes";
-import { InsertOneWriteOpResult } from "mongodb";
+import { InsertOneWriteOpResult, UpdateWriteOpResult, DeleteWriteOpResultObject } from "mongodb";
 
 @injectable()
 export class Games {
@@ -32,32 +32,32 @@ export class Games {
 
     public async resetLeaderboard(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
-            let game: Game = Object.assign(new Game, req.body);
-            // tslint:disable-next-line:prefer-for-of
-            let response = await this.mongo.updateDocumentById<Game>(Collections.Games, game._id,
-                                                                     {$set: {"leaderboards": game.leaderboards}});
+            const game: Game = Object.assign(new Game, req.body);
+            const response: UpdateWriteOpResult =
+                await this.mongo.updateDocumentById<Game>(Collections.Games, game._id, { $set: { "leaderboards": game.leaderboards } });
+
             if (response.result.ok) {
-                res.sendStatus(200);
+                res.sendStatus(CODES.OK);
             } else {
-                res.status(500).send("Failed to insert game into Mongo");
+                res.status(CODES.SERVER_ERROR).send("Failed to insert game into Mongo");
             }
         } catch (e) {
-            res.status(400).send("Game provided does not follow the valid format");
+            res.status(CODES.BAD_REQUEST).send("Game provided does not follow the valid format");
         }
     }
 
     public async deleteGame(req: Request, res: Response): Promise<void> {
         try {
-            let game: Game = Object.assign(new Game, req.body);
-            // tslint:disable-next-line:prefer-for-of
-            let response = await this.mongo.removeDocument<Game>(Collections.Games, game);
+            const game: Game = Object.assign(new Game, req.body);
+            const response: DeleteWriteOpResultObject = await this.mongo.removeDocument<Game>(Collections.Games, game);
+
             if (response.result.ok) {
-                res.sendStatus(200);
+                res.sendStatus(CODES.OK);
             } else {
-                res.status(500).send("Failed to delete game into Mongo");
+                res.status(CODES.SERVER_ERROR).send("Failed to delete game into Mongo");
             }
         } catch (e) {
-            res.status(400).send("Game provided does not follow the valid format");
+            res.status(CODES.BAD_REQUEST).send("Game provided does not follow the valid format");
         }
     }
 }
