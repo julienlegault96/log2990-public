cd8d6ca92a4a5946f26f6967b81df8843bf6dabe
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-28 14:44:07 -0400

    Refactor routes. KP

--- server/app/routes/abstract-route/abstract-route.ts
+++ server/app/routes/abstract-route/abstract-route.ts
@@ -1,24 +1,84 @@
 import { inject, injectable } from "inversify";
-import { InsertOneWriteOpResult, DeleteWriteOpResultObject } from "mongodb";
+import { InsertOneWriteOpResult, DeleteWriteOpResultObject, UpdateWriteOpResult } from "mongodb";
+import { Request, Response, NextFunction } from "express";
 
 import Types from "../../types";
 import { Mongo, Collections } from "../../services/mongo";
 
+import { CODES } from "../../../../common/communication/response-codes";
+
 @injectable()
-export class Games<T> {
 
-    public constructor(@inject(Types.Mongo) private mongo: Mongo) { }
+export class AbstractRoute<T extends Object> {
 
-    public async getAll(collection: Collections): Promise<T[]> {
-        return this.mongo.findDocuments<T>(collection);
+    protected collection: Collections;
+
+    public constructor(@inject(Types.Mongo) private mongo: Mongo) {
     }
 
-    public async insert(elem: T, collection: Collections): Promise<InsertOneWriteOpResult> {
-        return this.mongo.insertDocument<T>(collection, elem);
+    public async get(req: Request, res: Response, next: NextFunction): Promise<void> {
+        res.status(CODES.OK).send(JSON.stringify(await this.getAll()));
     }
 
-    public async remove(elem: T, collection: Collections): Promise<DeleteWriteOpResultObject> {
-        return this.mongo.removeDocument<T>(collection, elem);
+    public async post(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const elem: T = JSON.parse(req.body);
+            const response: InsertOneWriteOpResult = await this.insert(elem);
+
+            if (response.result.ok) {
+                res.sendStatus(CODES.OK);
+            } else {
+                res.status(CODES.SERVER_ERROR).send("Failed to insert into Mongo");
+            }
+        } catch (e) {
+            res.status(CODES.BAD_REQUEST).send("Element provided does not follow the valid format");
+        }
+    }
+
+    public async updateById(req: Request, res: Response, next: NextFunction, id: number): Promise<void> {
+        try {
+            const elem: T = JSON.parse(req.body);
+            const response: UpdateWriteOpResult = await this.update(id, elem);
+
+            if (response.result.ok) {
+                res.sendStatus(CODES.OK);
+            } else {
+                res.status(CODES.SERVER_ERROR).send("Failed to insert game into Mongo");
+            }
+        } catch (e) {
+            res.status(CODES.BAD_REQUEST).send("Game provided does not follow the valid format");
+        }
+    }
+
+    public async delete(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const elem: T = JSON.parse(req.body);
+            const response: DeleteWriteOpResultObject = await this.remove(elem);
+
+            if (response.result.ok) {
+                res.sendStatus(CODES.OK);
+            } else {
+                res.status(CODES.SERVER_ERROR).send("Failed to remove user from Mongo");
+            }
+        } catch (e) {
+            res.status(CODES.BAD_REQUEST).send("User provided does not follow the valid format");
+        }
+    }
+
+    public async getAll(): Promise<T[]> {
+        return this.mongo.findDocuments<T>(this.collection);
+    }
+
+    public async insert(elem: T): Promise<InsertOneWriteOpResult> {
+        return this.mongo.insertDocument<T>(this.collection, elem);
+    }
+
+    private async update(id: number, elem: T): Promise<UpdateWriteOpResult> {
+        return this.mongo.updateDocumentById<T>(Collections.Games, id, elem);
+    }
+
+    public async remove(elem: T): Promise<DeleteWriteOpResultObject> {
+        return this.mongo.removeDocument<T>(this.collection, elem);
     }
 
 }
--- server/app/routes/users.ts
+++ server/app/routes/users.ts
@@ -1,60 +1,16 @@
-import { Request, Response, NextFunction } from "express";
 import { inject, injectable } from "inversify";
 
 import Types from "../types";
 import { User } from "../../../common/user/user";
-import { CODES } from "../../../common/communication/response-codes";
 import { Mongo, Collections } from "../services/mongo";
-import { InsertOneWriteOpResult, DeleteWriteOpResultObject } from "mongodb";
+import { AbstractRoute } from "./abstract-route/abstract-route";
 
 @injectable()
-export class Users {
+export class Users extends AbstractRoute<User> {
 
-    public constructor(@inject(Types.Mongo) private mongo: Mongo) { }
-
-    public async findUsers(): Promise<User[]> {
-        return this.mongo.findDocuments<User>(Collections.Users);
+    public constructor(@inject(Types.Mongo) mongo: Mongo) {
+        super(mongo);
+        this.collection = Collections.Users;
     }
 
-    public async insertUser(user: User): Promise<InsertOneWriteOpResult> {
-        return this.mongo.insertDocument<User>(Collections.Users, user);
-    }
-
-    public async removeUser(user: User): Promise<DeleteWriteOpResultObject> {
-        return this.mongo.removeDocument<User>(Collections.Users, user);
-    }
-
-    public async getUsers(req: Request, res: Response, next: NextFunction): Promise<void> {
-        res.status(CODES.OK).send(JSON.stringify(await this.findUsers()));
-    }
-
-    public async postUser(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-
-            const response: InsertOneWriteOpResult = await this. insertUser(Object.assign(new User, req.body));
-
-            if (response.result.ok) {
-                res.sendStatus(CODES.OK);
-            } else {
-                res.status(CODES.SERVER_ERROR).send("Failed to insert user into Mongo");
-            }
-        } catch (e) {
-            res.status(CODES.BAD_REQUEST).send("User provided does not follow the valid format");
-        }
-    }
-
-    public async deleteUsers(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-
-            const response: DeleteWriteOpResultObject = await this.removeUser(Object.assign(new User, req.body));
-
-            if (response.result.ok) {
-                res.sendStatus(CODES.OK);
-            } else {
-                res.status(CODES.SERVER_ERROR).send("Failed to remove user from Mongo");
-            }
-        } catch (e) {
-            res.status(CODES.BAD_REQUEST).send("User provided does not follow the valid format");
-        }
-    }
 }
