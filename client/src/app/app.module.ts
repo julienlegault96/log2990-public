0b843e571ea828b709f55a7b6a5e04aba78cbe47
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-09-28 12:12:55 -0400

    Organisation app module. KP

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from ".//app-routing.module";
+import { HomePageComponent } from "./home-page/home-page.component";
+
+import { GameService } from "./services/game.service";
 import { GameListComponent } from "./game/game-list-menu/game-list/game-list.component";
 import { GameCardComponent } from "./game/game-list-menu/game-card/game-card.component";
 import { LeaderboardComponent } from "./game/game-list-menu/leaderboard/leaderboard.component";
+
 import { SoloGameComponent } from "./game/solo-game/solo-game.component";
 import { ChronoComponent } from "./game/chrono/chrono.component";
-import { FormsModule } from "@angular/forms";
+
 import { UserComponent } from "./User/user-component/user.component";
-import { CreateGameComponent } from "./game/create-game/create-game.component";
-import { GameService } from "./services/game.service";
-import { CreateGameService } from "./services/create-game.service";
+
 import { AdminViewComponent } from "./admin/admin-view/admin-view.component";
 import { AdminViewCardComponent } from "./admin/admin-view-card/admin-view-card.component";
-import { HomePageComponent } from "./home-page/home-page.component";
+
+import { CreateGameService } from "./services/create-game.service";
+import { CreateGameComponent } from "./game/create-game/create-game.component";
 
 @NgModule({
     declarations: [
         AppComponent,
+        HomePageComponent,
         GameListComponent,
         GameCardComponent,
         LeaderboardComponent,
@@ -30,13 +36,12 @@ import { HomePageComponent } from "./home-page/home-page.component";
         AdminViewComponent,
         AdminViewCardComponent,
         CreateGameComponent,
-        HomePageComponent
     ],
     imports: [
+        AppRoutingModule,
         BrowserModule,
         HttpClientModule,
-        AppRoutingModule,
-        FormsModule
+        FormsModule,
     ],
     providers: [
         GameService,
