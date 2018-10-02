4c2bff2a9acfd057cc1db18e3380321d0cb2f001
Louis Sérey Larose <papalarose@yahoo.ca>
2018-10-02 14:15:35 -0400

    tests pass and component builds correctly
    LSLarose

--- client/src/app/diff-counter/diff-counter.component.spec.ts
+++ client/src/app/diff-counter/diff-counter.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { DiffCounterComponent } from "./diff-counter.component";
+import { catchError } from "rxjs/operators";
 
 describe("DiffCounterComponent", () => {
     let component: DiffCounterComponent;
@@ -24,32 +25,38 @@ describe("DiffCounterComponent", () => {
     });
 
     it("devrais lire 0 à l'initialisation pour un joueur", () => {
-        component.playerOneId = "playerOneId";
-        expect(component.counters[0]).toEqual(0);
+        component.setPlayerOne("playerOneId");
+
+        expect(component.getPlayerCount("playerOneId")).toEqual(0);
     });
 
     it("devrais contenir autant de counter que de joueurs", () => {
-        component.playerOneId = "playerOneId";
-        component.playerTwoId = "playerTwoId";
-        fixture.detectChanges();
+        component.setPlayerOne("playerOneId");
 
-        const arrayLength: number = 2;
-        expect(component.counters.length).toEqual(arrayLength);
+        expect(component.getPlayerCount("playerOneId")).toEqual(0);
+        try {
+            component.getPlayerCount("playerTwoId");
+        } catch (e) {
+            expect(e instanceof Error).toBeTruthy();
+        }
+
+        component.setPlayerTwo("playerTwoId");
+        expect(component.getPlayerCount("playerTwoId")).toEqual(0);
     });
 
     it("devrais lire 0 à l'initialisation pour deux joueur", () => {
-        component.playerOneId = "playerOneId";
-        component.playerTwoId = "playerTwoId";
-        fixture.detectChanges();
+        component.setPlayerOne("playerOneId");
+        component.setPlayerTwo("playerTwoId");
 
-        expect(component.counters[0]).toEqual(0);
-        expect(component.counters[1]).toEqual(0);
+        expect(component.getPlayerCount("playerOneId")).toEqual(0);
+        expect(component.getPlayerCount("playerTwoId")).toEqual(0);
     });
 
     it("le compteur devrais s'incrémenter de 1 à chaque différence trouvée", () => {
-        component.playerOneId = "playerOneId";
+        component.setPlayerOne("playerOneId");
         component.diffFound("playerOneId");
-        expect(component.counters[0]).toBe(1);
+
+        expect(component.getPlayerCount("playerOneId")).toEqual(1);
     });
 
 });
