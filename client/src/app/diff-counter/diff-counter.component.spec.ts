4ad0544c5af0a8991d61317664b00777aa0c1a65
Louis Sérey Larose <papalarose@yahoo.ca>
2018-10-02 14:20:46 -0400

    removed useless import in diff counter specs
    LSLarose

--- client/src/app/diff-counter/diff-counter.component.spec.ts
+++ client/src/app/diff-counter/diff-counter.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { DiffCounterComponent } from "./diff-counter.component";
-import { catchError } from "rxjs/operators";
 
 describe("DiffCounterComponent", () => {
     let component: DiffCounterComponent;
