4d52e043134b86151d0a17b47ed4ffbb0177b145
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-10-05 09:53:54 -0400

    Completion methodes. KP et LSL

--- server/app/img-diff-generator/img-diff-generator/Image.cpp
+++ server/app/img-diff-generator/img-diff-generator/Image.cpp
@@ -1,9 +1,10 @@
 #include "Image.h"
 
-Image::Image(const unsigned & height, const unsigned & width)
+Image::Image(const unsigned & height, const unsigned & width) : pixels(width, vector<Pixel>(height))
 {
 }
 
 void Image::setPixel(const unsigned & x, const unsigned & y, const Pixel & pixel)
 {
+	pixels[x][y] = pixel;
 }
