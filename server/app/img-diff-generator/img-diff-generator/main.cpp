1bcc48a7655a531c1457a9891bd61629adbd4443
Louis Sérey Larose <papalarose@yahoo.ca>
2018-10-13 15:32:55 -0400

    ajout d'un type d'erreur, définition de l'emplacement des images
    LSLarose

--- server/app/img-diff-generator/img-diff-generator/main.cpp
+++ server/app/img-diff-generator/img-diff-generator/main.cpp
@@ -7,38 +7,44 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	string filename = "C:\\Users\\Kevin\\Documents\\PolyMTL\\Session 4\\LOG2990\\log2990\\server\\app\\img-diff-generator\\Debug\\test3.bmp";
-	
-	//cout << "Enter filename: " << endl;
-	//cin >> filename;
+	const char* filename = "C:\\Users\\papal\\Documents\\Travail\\LOG2990 - Projet 2\\server\\app\\img-diff-generator\\img-diff-generator\\Debug\\diff.bmp";
+
 	try
 	{
-		string filename1 = "C:\\Users\\Kevin\\Documents\\PolyMTL\\Session 4\\LOG2990\\log2990\\server\\app\\img-diff-generator\\Debug\\comp1.bmp";
-		string filename2 = "C:\\Users\\Kevin\\Documents\\PolyMTL\\Session 4\\LOG2990\\log2990\\server\\app\\img-diff-generator\\Debug\\comp2.bmp";
+		const char* filename1 =
+			"C:\\Users\\papal\\Documents\\Travail\\LOG2990 - Projet 2\\server\\app\\img-diff-generator\\img-diff-generator\\Debug\\comp1.bmp";
+			//"C:\\Users\\Kevin\\Documents\\PolyMTL\\Session 4\\LOG2990\\log2990\\server\\app\\img-diff-generator\\Debug\\comp1.bmp";
+		const char* filename2 =
+			"C:\\Users\\papal\\Documents\\Travail\\LOG2990 - Projet 2\\server\\app\\img-diff-generator\\img-diff-generator\\Debug\\comp2.bmp";
+			//"C:\\Users\\Kevin\\Documents\\PolyMTL\\Session 4\\LOG2990\\log2990\\server\\app\\img-diff-generator\\Debug\\comp2.bmp";
 		Comparator comparator;
-		comparator.exec(filename1, filename2);
-		comparator.save(filename);
+		comparator.compare(filename1, filename2);
+		comparator.saveTo(filename);
 	}
-	catch (const std::runtime_error & e)
-	{
+	catch (const std::runtime_error & e)	{
 		cerr << "Runtime error:" << endl;
 		cerr << e.what() << endl;
+		return -1;
 	}
-	catch (const std::invalid_argument & e)
-	{
+	catch (const std::invalid_argument & e)	{
 		cerr << "Invalid argument: " << endl;
 		cerr << e.what() << endl;
+		return -1;
 	}
-	catch (const std::exception & e)
-	{
+	catch (const std::out_of_range & e)	{
+		cerr << "Tried to access out of declared scope: " << endl;
+		cerr << e.what() << endl;
+		return -1;
+	}
+	catch (const std::exception & e)	{
 		cerr << "Generic exception: " << endl;
 		cerr << e.what() << endl;
+		return -1;
 	}
-	catch (...)
-	{
+	catch (...)	{
 		cerr << "Unknown exception" << endl;
+		return -1;
 	}
-	//TODO take away
-	system("pause");
+
 	return 0;
 }
