ab9db77086da220297cee67ee83946766bf19254
Louis Sérey Larose <papalarose@yahoo.ca>
2018-10-19 10:51:59 -0400

    added base64 encoder/decoder from the internets
    LSLarose

---
+++ server/app/img-diff-generator/img-diff-generator/base64.cpp
@@ -0,0 +1,86 @@
+#include "base64.h"
+
+/**
+* base64_encode - Base64 encode
+* @src: Data to be encoded
+* @len: Length of the data to be encoded
+* @out_len: Pointer to output length variable, or %NULL if not used
+* Returns: Allocated buffer of out_len bytes of encoded data,
+* or empty string on failure
+*/
+const std::string base64::encode(const unsigned char *src, size_t len)
+{
+	unsigned char *out, *pos;
+	const unsigned char *end, *in;
+
+	size_t olen;
+
+	olen = 4 * ((len + 2) / 3); /* 3-byte blocks to 4-byte */
+
+	if (olen < len)
+		return std::string(); /* integer overflow */
+
+	std::string outStr;
+	outStr.resize(olen);
+	out = (unsigned char*)&outStr[0];
+
+	end = src + len;
+	in = src;
+	pos = out;
+	while (end - in >= 3) {
+		*pos++ = base64_table[in[0] >> 2];
+		*pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+		*pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+		*pos++ = base64_table[in[2] & 0x3f];
+		in += 3;
+	}
+
+	if (end - in) {
+		*pos++ = base64_table[in[0] >> 2];
+		if (end - in == 1) {
+			*pos++ = base64_table[(in[0] & 0x03) << 4];
+			*pos++ = '=';
+		}
+		else {
+			*pos++ = base64_table[((in[0] & 0x03) << 4) |
+				(in[1] >> 4)];
+			*pos++ = base64_table[(in[1] & 0x0f) << 2];
+		}
+		*pos++ = '=';
+	}
+
+	return outStr;
+}
+
+
+const std::string base64::decode(const void* data, const size_t &len)
+{
+	if (len == 0) return "";
+
+	unsigned char *p = (unsigned char*)data;
+	size_t j = 0,
+		pad1 = len % 4 || p[len - 1] == '=',
+		pad2 = pad1 && (len % 4 > 2 || p[len - 2] != '=');
+	const size_t last = (len - pad1) / 4 << 2;
+	std::string result(last / 4 * 3 + pad1 + pad2, '\0');
+	unsigned char *str = (unsigned char*)&result[0];
+
+	for (size_t i = 0; i < last; i += 4)
+	{
+		int n = B64index[p[i]] << 18 | B64index[p[i + 1]] << 12 | B64index[p[i + 2]] << 6 | B64index[p[i + 3]];
+		str[j++] = n >> 16;
+		str[j++] = n >> 8 & 0xFF;
+		str[j++] = n & 0xFF;
+	}
+	if (pad1)
+	{
+		int n = B64index[p[last]] << 18 | B64index[p[last + 1]] << 12;
+		str[j++] = n >> 16;
+		if (pad2)
+		{
+			n |= B64index[p[last + 2]] << 6;
+			str[j++] = n >> 8 & 0xFF;
+		}
+	}
+	return result;
+}
---
+++ server/app/img-diff-generator/img-diff-generator/base64.h
@@ -0,0 +1,34 @@
+#pragma once
+
+/*
+* Base64 encoding/decoding (RFC1341)
+* Copyright (c) 2005-2011, Jouni Malinen <j@w1.fi>
+*
+* This software may be distributed under the terms of the BSD license.
+* See README for more details.
+*/
+
+// 2016-12-12 - Gaspard Petit : Slightly modified encode to return a std::string 
+// instead of a buffer allocated with malloc.
+
+// 2018-10-19 - Louis Sérey Larose : added decoder from Polsofol's answer at
+//https://stackoverflow.com/questions/180947/base64-decode-snippet-in-c/13935718
+
+#include <string>
+
+class base64 {
+public:
+	const std::string encode(const unsigned char *src, size_t len);
+	const std::string decode(const void* data, const size_t &len);
+private:
+	const unsigned char base64_table[65] =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+	const int B64index[256] = { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+		0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+		0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 62, 63, 62, 62, 63, 52, 53, 54, 55,
+		56, 57, 58, 59, 60, 61,  0,  0,  0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,
+		7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,  0,
+		0,  0,  0, 63,  0, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+		41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 };
+};
