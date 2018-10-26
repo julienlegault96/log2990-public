40defe0ce4303be8fe0be079a40570dbda1a51fe
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-10-26 10:40:11 -0400

    Tests Imgur. kp

---
+++ server/app/routes/imgur/imgur.spec.ts
@@ -0,0 +1,23 @@
+import { expect } from "chai";
+import { Imgur } from "./imgur";
+
+describe("Imgdiff service", () => {
+    // set up fixtures
+
+    const imgur: Imgur = new Imgur();
+
+    it("should parse base64", async () => {
+        const data: string = "data:image/bmp;base64,-----";
+        const parsedData: string = imgur["parseBase64"](data);
+
+        expect(parsedData).to.be.equal("-----");
+    });
+
+    it("should parse base64", async () => {
+        const data: string = "-----";
+        const parsedData: string = imgur["parseBase64"](data);
+
+        expect(parsedData).to.be.equal("-----");
+    });
+
+});
