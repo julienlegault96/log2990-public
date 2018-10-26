4e5e4f2b684a0a61e053fb77f3c6c99db853ea66
KevinPastor <kevin.pastor@polymtl.ca>
2018-10-25 23:00:37 -0400

    New games tests. KP - GB

--- server/app/routes/games.spec.ts
+++ server/app/routes/games.spec.ts
@@ -1,70 +1,27 @@
 import { expect } from "chai";
-import { Game } from "../../../common/game/game";
-import { GAMES } from "../../../common/game/mock-games";
 import { Games } from "./games";
 import { Mongo } from "../services/mongo";
-import { InsertOneWriteOpResult, DeleteWriteOpResultObject } from "mongodb";
 
 describe("Games db services", () => {
     // set up fixtures
     const games: Games = new Games(new Mongo());
-    let serverGames: Game[];
 
-    it("should fetch and delete all games", async function (): Promise<void> {
-        // l'appel prend plus de 2000ms
-        // tslint:disable-next-line:no-invalid-this no-magic-numbers
-        this.timeout(10000);
-        serverGames = await games.getAll();
-        for (const iterator of serverGames) {
-            await games.remove(iterator);
-        }
+    it("should generate game id", async () => {
+        const id: number = games["generateId"]();
 
-        const getResponse: Game[] = await games.getAll();
-        expect(getResponse.length).to.equal(0);
+        expect(id).to.be.a("number");
+        expect(id).to.be.at.least(0);
+        expect(id).to.be.at.most(games["ID_RANGE"]);
     });
 
-    it("should create mock game", async () => {
-        // insert
-        const insResponse: InsertOneWriteOpResult = await games.insert(GAMES[0]);
-
-        expect(insResponse.insertedCount).to.equal(1);
+    it("should not have valid error count", async () => {
+        const result: boolean = await games["hasValidDifferenceCount"]("./test_assets/oneError.bmp");
+        expect(result).to.equal(false);
     });
 
-    it("shouldn't create mock game a second time", async () => {
-        // insert
-        try {
-            await games.insert(GAMES[0]);
-        } catch (err) {
-            expect(String(err).includes("duplicate key error")).to.equal(true);
-        }
+    it("should have valid error count", async () => {
+        const result: boolean = await games["hasValidDifferenceCount"]("./test_assets/sevenErrors.bmp");
+        expect(result).to.equal(true);
     });
 
-    it("should fetch mock game", async () => {
-        // get
-        const getResponse: Game[] = await games.getAll();
-
-        expect(getResponse.length).to.equal(1);
-        expect(getResponse[0].title).to.equal(GAMES[0].title);
-    });
-
-    it("should delete mock game", async () => {
-        // delete
-        const delResponse: DeleteWriteOpResultObject = await games.remove(GAMES[0]);
-
-        expect(Number(delResponse.deletedCount)).to.equal(1);
-    });
-
-    it("should create back all serverGames", async function(): Promise<void> {
-        // l'appel prend plus de 2000ms
-        // tslint:disable-next-line:no-invalid-this no-magic-numbers
-        this.timeout(10000);
-
-        for (const iterator of serverGames) {
-            await games.insert(iterator);
-        }
-
-        const getResponse: Game[] = await games.getAll();
-        expect(getResponse.length).to.equal(serverGames.length);
-
-    });
 });
