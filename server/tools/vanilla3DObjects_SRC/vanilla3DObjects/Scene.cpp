d9b5344322d4b80f3ad47c4ebacd58ded8eaec31
Louis Sérey Larose <papalarose@yahoo.ca>
2018-11-07 01:38:42 -0500

    update de la classe scene pour accepter le drawer
    LSLarose

--- server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Scene.cpp
+++ server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Scene.cpp
@@ -1,13 +1,15 @@
 #include"Scene.h"
+#include "GeoFactory.h"
+#include "ThemeFactory.h"
 
 bool Scene::getTheme() const
 {
-    return _theme;
+    return theme_;
 }
-Scene::Scene(int numberShapes, double dimBoite, bool theme) : _numberShapes(numberShapes), _dimBoite(dimBoite), _theme(theme)
+Scene::Scene(int numberShapes, bool theme) : numberShapes_(numberShapes), theme_(theme)
 {
-    calculateScalingFactor();
-    generateShapes();
+    factory_ = theme_ ? ThemeFactory() : GeoFactory(objects_, dimBoite_);
+    factory_->generateShape(numberShapes_);
 }
 
 void Scene::parseModOptions(const std::string & optionString) {
@@ -17,103 +19,25 @@ void Scene::parseModOptions(const std::string & optionString) {
 }
 
 Scene::~Scene() {
-    // call destructor for each contained shape
-    _shapes.clear();
+    objects_.clear();
 }
 
-void Scene::generateShapes() {
-    for (int index = 0; index < _numberShapes; index++)
-    {
-        generateShape();
-    }
-}
-
-void Scene::generateShape()
-{
-    if (this->_theme == false) {
-        Shapelist type = Shapelist(rand() % Shapelist::Cylindre + 1);
-        glm::vec4 color(randFloat(0, 1), randFloat(0, 1), randFloat(0, 1), 1);
-
-        glm::vec3 translateShape(0, 0, 0);
-        randCoords(&translateShape);
-
-        glm::vec3 RotateShape(randFloat(0, 1), randFloat(0, 1), randFloat(0, 1));
-
-        GLfloat scaleShape = randFloat(MIN_SIZE_MODIFIER*_scalingFactor, MAX_SIZE_MODIFIER*_scalingFactor);
-        Shape *newShape = new Shape(type, translateShape, color, randFloat(0, 360), RotateShape, scaleShape);
-        _shapes.push_back(newShape);
-    }
-    else {
-
-        ShapeThemelist type = ShapeThemelist(rand() % ShapeThemelist::Saturn + 1);
-        glm::vec4 color(0.5, 0.5, 0.5, 1);
-
-        glm::vec3 translateShape(0, 0, 0);
-        randCoords(&translateShape);
-
-        glm::vec3 RotateShape(randFloat(0, 1), randFloat(0, 1), randFloat(0, 1));
-
-        GLfloat scaleShape = randFloat(MIN_SIZE_MODIFIER * _scalingFactor, MAX_SIZE_MODIFIER * _scalingFactor);
-        Planet *newPlanet = new Planet(type, translateShape, color, randFloat(0, 360), RotateShape, scaleShape);
-        _planets.push_back(newPlanet);
-    }
-
-}
-
-//fonction retournant un float aleatoire
-//source : https://www.gamedev.net/forums/topic/41147-random-glfloat-value/
-float Scene::randFloat(const float& min, const float& max) const {
-    float range = max - min;
-    float num = range * rand() / RAND_MAX;
-    return (num + min);
-}
-
-void Scene::randCoords(glm::vec3 *coords) {
-    do {
-        coords->x = randFloat(-_dimBoite / 2, _dimBoite / 2);
-        coords->y = randFloat(-_dimBoite / 2, _dimBoite / 2);
-        coords->z = randFloat(0, _dimBoite - 1);
-    } while (checkForCollision(coords));
-}
-
-bool Scene::checkForCollision(glm::vec3 *coords) {
-    bool collision = false;
-    double distance = 0;
-    for (Shape* shape : _shapes)
-    {
-        distance =
-            pow(shape->coords_.x - coords->x, 2) +
-            pow(shape->coords_.y - coords->y, 2) +
-            pow(shape->coords_.z - coords->z, 2);
-
-        collision = distance < MIN_DISTANCE * _scalingFactor;
-        if (collision) { break; }
-    }
-    return collision;
-}
-
-std::vector<Shape*> Scene::getShapes() const {
-    return _shapes;
-}
-
-
-std::vector<Planet*> Scene::getPlanets() const {
-    return _planets;
+std::vector<AbstractShape*> Scene::getObjects() const {
+    return objects_;
 }
 
 void Scene::modify()
 {
-
     if (_banString.length() == Modifications::DeleteObject + 1)
     {
         return /*all modifications banned*/;
     }
     for (int i = 0; i < MOD_COUNT; i++)
     {
-        int index = rand() % this->_numberShapes;
+        int index = rand() % this->numberShapes_;
         // only modify unmodified shapes
-        while (this->_shapes.at(index)->modified_) {
-            index = rand() % this->_numberShapes;
+        while (this->objects_.at(index)->isModded()) {
+            index = rand() % this->numberShapes_;
         }
 
         Modifications mod = Modifications(rand() % (Modifications::DeleteObject + 1));
@@ -140,23 +64,18 @@ void Scene::modify()
 }
 
 void Scene::changeColor(int index) {
-    glm::vec4 newColor(randFloat(0, 1), randFloat(0, 1), randFloat(0, 1), 1);
-    while (this->_shapes.at(index)->baseColor_ == newColor) {
-        glm::vec4 newColor(randFloat(0, 1), randFloat(0, 1), randFloat(0, 1), 1);
-    }
-    _shapes.at(index)->ChangeColor(newColor);
+    glm::vec4 newColor;
+    do {
+        newColor = glm::vec4(factory_->generateFloat(0, 1), factory_->generateFloat(0, 1), factory_->generateFloat(0, 1), 1);
+    } while (this->objects_.at(index)->getColor() == newColor);
+    objects_.at(index)->setColor(newColor);
 }
 
 void Scene::deleteShape(int index) {
-    _shapes.at(index)->hide();
+    objects_.at(index)->hide();
 }
 
 void Scene::addShape() {
-    calculateScalingFactor();
-    generateShape();
-    _shapes.at(_numberShapes++)->modified_ = true;
-}
-
-void Scene::calculateScalingFactor() {
-    _scalingFactor = pow(_dimBoite, 3) / (ALLOCATED_SHAPE_VOLUME * _numberShapes);
+    factory_->generateShape();
+    objects_.at(numberShapes_++)->setModified();
 }
--- server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Scene.h
+++ server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Scene.h
@@ -1,46 +1,40 @@
 #pragma once
+
 #include <cmath>
 #include <string>
+#include <vector>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
-#include "Shape.h"
-#include "Planet.h"
-#include <math.h>
+#include "Default3DProgramState.h"
+#include "AbstractShape.h"
+#include "AbstractFactory.h"
+#include "Drawer.h"
+
+
 enum Modifications { ColorChange, AddObject, DeleteObject};
 class Scene {
 public:
-  Scene(int numberShapes, double dimBoite, bool theme);
+  Scene(int numberShapes, bool theme);
   ~Scene();
   void modify();
   void parseModOptions(const std::string &optionString);
-  std::vector<Shape*> getShapes() const;
-  std::vector<Planet*> getPlanets() const;
-  bool getTheme() const;
+  std::vector<AbstractShape*> getObjects() const;
+  bool isThematic() const;
+  void accept(Drawer& visitor);
 
 private:
-	float const MIN_SIZE_MODIFIER = 0.5;
-	float const MAX_SIZE_MODIFIER = 1.5;
     short const MOD_COUNT = 7;
-	short const MIN_DISTANCE = 12;
-	short const ALLOCATED_SHAPE_VOLUME = 49;
     const char ADD_PARAMETER = 'a';
     const char SUP_PARAMETER = 's';
     const char COL_PARAMETER = 'c';
     std::string _banString;
-    std::vector<Shape*> _shapes = {};
-    std::vector<Planet*> _planets = {};
-    bool _theme;
-    int _numberShapes = 0;
-	double _scalingFactor = 1;
-	double _dimBoite;
+    AbstractFactory* factory_;
+    std::vector<AbstractShape*> objects_ = {};
+    bool theme_;
+    int numberShapes_ = 0;
+	double dimBoite_;
 
-    void generateShapes();
-    float randFloat(const float& min, const float& max) const;
-	void randCoords(glm::vec3 *coords);
-	bool checkForCollision(glm::vec3 *coords);
 	void changeColor(int index);
 	void deleteShape(int index);
 	void addShape();
-	void generateShape();
-	void calculateScalingFactor();
 };
