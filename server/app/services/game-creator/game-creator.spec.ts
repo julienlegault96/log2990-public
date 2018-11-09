ed04292edd1d837b056c44f4e418b49153d6990d
KevinPastor <kevin.pastor@polymtl.ca>
2018-11-08 22:27:40 -0500

    Added tests. KP

---
+++ server/app/services/game-creator/game-creator.spec.ts
@@ -0,0 +1,21 @@
+import { expect } from "chai";
+import { GameCreator } from "./game-creator";
+
+describe("Game creator service", () => {
+
+    let gameCreator: GameCreator;
+    beforeEach(() => {
+        gameCreator = new GameCreator();
+    });
+
+    it("should not have valid generated images", async () => {
+        const result: boolean = await gameCreator["isValidGeneratedImages"](["", "", "", "", "", ""]);
+        expect(result).to.equal(false);
+    });
+
+    it("should have valid generated images", async () => {
+        const result: boolean = await gameCreator["isValidGeneratedImages"](["", "", "a", "", "", "a"]);
+        expect(result).to.equal(true);
+    });
+
+});
