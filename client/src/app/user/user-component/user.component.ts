852ca6eaaeb570d38bd85009a1d8591a3a18438d
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-11-10 09:39:10 -0500

    Revamp UI entry point. KP

--- client/src/app/user/user-component/user.component.ts
+++ client/src/app/user/user-component/user.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 
 import { UserService } from "../../services/user.service";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "app-user-component",
@@ -14,13 +15,14 @@ export class UserComponent {
 
     private username: string;
 
-    public constructor(private userService: UserService) {
+    public constructor(private userService: UserService, private router: Router) {
         this.username = "";
     }
 
     public submit(): void {
         try {
             this.userService.submitUsername(this.username);
+            this.router.navigate(["/", "gameList"]);
         } catch (error) {
             alert(error);
         }
