cc158636af47843b8920c601226ec90244007333
Julien Legault <julien-3.legault@polymtl.ca>
2018-11-13 18:28:15 -0500

    ajout de la classe soleil

---
+++ server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Sun.cpp
@@ -0,0 +1,112 @@
+#include "Sun.h"
+#include "Shape.h"
+
+Sunny::Sunny(glm::vec3 coords, glm::vec3 rotationAxis, GLfloat rotation, GLfloat scale)
+	: CompositeShape(coords, rotationAxis, rotation, scale)
+{
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Sphere,
+			GRIS_CLAIR_POUR_TEXTURES,
+			coords_,
+			rotationAxis_,
+			rotation_,
+			scale_ * (GLfloat) 1.0
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Tetrahedre,
+			GRIS_CLAIR_POUR_TEXTURES,
+			coords_ + glm::vec3(0.0, 0.0, scale_/2),
+			rotationAxis_ + glm::vec3(0.0, 0.0, 7),
+			rotation_,
+			scale_* (GLfloat) 0.5
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Tetrahedre,
+			GRIS_CLAIR_POUR_TEXTURES,
+			coords_ + glm::vec3(0.0, -scale_ /2.5, 0.0),
+			rotationAxis_ + glm::vec3(0.0, 0.0, 7),
+			rotation_,
+			scale_* (GLfloat) 0.5
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Tetrahedre,
+			GRIS_CLAIR_POUR_TEXTURES,
+			coords_ + glm::vec3(0.0, scale_ / 2.5, 0.0),
+			rotationAxis_ + glm::vec3(0.0, 0.0, 7),
+			rotation_,
+			scale_* (GLfloat) 0.5
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Tetrahedre,
+			GRIS_CLAIR_POUR_TEXTURES,
+			coords_ + glm::vec3(scale_ /3, scale_ /4, 0.0),
+			rotationAxis_ + glm::vec3(0.0, 0.0, 7),
+			rotation_,
+			scale_* (GLfloat) 0.5
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Tetrahedre,
+			GRIS_CLAIR_POUR_TEXTURES,
+			coords_ + glm::vec3(-scale_ / 5, -scale_ / 5, -scale_ / 5),
+			rotationAxis_ + glm::vec3(0.0, 0.0, 7),
+			rotation_,
+			scale_* (GLfloat) 0.5
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Tetrahedre,
+			GRIS_CLAIR_POUR_TEXTURES,
+			coords_ + glm::vec3(scale_ / 5, -scale_ / 5, -scale_ / 5),
+			rotationAxis_ + glm::vec3(0.0, 0.0, 7),
+			rotation_,
+			scale_* (GLfloat) 0.5
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Tetrahedre,
+			GRIS_CLAIR_POUR_TEXTURES,			
+			coords_ + glm::vec3(-scale_ / 4, -scale_ / 4, scale_ / 4),
+			rotationAxis_ + glm::vec3(0.0, 0.0, 7),
+			rotation_,
+			scale_* (GLfloat) 0.5
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Tetrahedre,
+			//GRIS_CLAIR_POUR_TEXTURES,
+			glm::vec4(0.5, 0.5, 0.5, 1.0), //gris
+			coords_ + glm::vec3(-scale_ / 4, scale_ / 4, scale_ / 4),
+			rotationAxis_ + glm::vec3(0.0, 0.0, 7),
+			rotation_,
+			scale_* (GLfloat) 0.5
+		)
+	);
+
+
+
+}
+Sunny::~Sunny()
+{
+}
---
+++ server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Sun.h
@@ -0,0 +1,13 @@
+#ifndef SUN_H
+#define SUN_H
+
+#include "CompositeShape.h"
+class Sunny :
+	public CompositeShape
+{
+public:
+	Sunny(glm::vec3 coords, glm::vec3 rotationAxis, GLfloat rotation, GLfloat scale);
+	~Sunny();
+};
+
+#endif
