9395091839b352d3c3bc667bda4568066b0bccf3
Louis Sérey Larose <papalarose@yahoo.ca>
2018-11-14 12:31:14 -0500

    added a choice reroll for coherent thematic choices
    LSLarose

--- server/tools/vanilla3DObjects_SRC/vanilla3DObjects/ThemeFactory.h
+++ server/tools/vanilla3DObjects_SRC/vanilla3DObjects/ThemeFactory.h
@@ -12,6 +12,8 @@ private:
     enum possibleShapes { asteroid, planet, flyingSaucer, alienShip, enumSize, sun, fusee, heatShield, spaceship };
 	short const MIN_DISTANCE = 12;
 	bool isSun_ = false;
+
+    short generateCoherentContentChoice() const;
 };
 #endif
 
