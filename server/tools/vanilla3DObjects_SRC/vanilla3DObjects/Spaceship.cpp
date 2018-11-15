eed5241537fef3120409c567cc949e127a8a9f4a
Julien Legault <julien-3.legault@polymtl.ca>
2018-11-15 02:15:06 -0500

    ajout d ailles et changement de couleur su spaceship

--- server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Spaceship.cpp
+++ server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Spaceship.cpp
@@ -4,15 +4,26 @@
 Spaceship::Spaceship(glm::vec3 coords, glm::vec3 rotationAxis, GLfloat rotation, GLfloat scale)
 	: CompositeShape(coords, rotationAxis, rotation, scale)
 {
+		shapes_.push_back(
+			new Shape(
+				Shapelist::Theiere,
+				glm::vec4(0.4, 0.4, 0.4, 1.0),
+				glm::vec3(0, 0, 0),
+				rotationAxis_,
+				(GLfloat)0,
+				(GLfloat) 0.1
+			)
+		);
+
 	shapes_.push_back(
 		new Shape(
-			Shapelist::Theiere,
-			GRIS_CLAIR_POUR_TEXTURES,
-			coords_,
-			rotationAxis_,
-			rotation_,
-			scale_ * (GLfloat) 0.10
+			Shapelist::Cube,
+			glm::vec4(0.1, 0.1, 0.1, 1.0),
+			glm::vec3(0., 0., 0.2),
+			glm::vec3(1., 0., 0.),
+			(GLfloat)5,
+			(GLfloat) 0.8,
+			glm::vec3(0.5, 1.0, 0.1)
 		)
 	);
-
 }
