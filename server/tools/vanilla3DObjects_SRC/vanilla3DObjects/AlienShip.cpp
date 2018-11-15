0f1a82263f847d7419174f8b2e680decf31847a6
Julien Legault <julien-3.legault@polymtl.ca>
2018-11-15 02:47:58 -0500

    ajout de 'pattes' au alienShip

--- server/tools/vanilla3DObjects_SRC/vanilla3DObjects/AlienShip.cpp
+++ server/tools/vanilla3DObjects_SRC/vanilla3DObjects/AlienShip.cpp
@@ -7,12 +7,37 @@ AlienShip::AlienShip(glm::vec3 coords, glm::vec3 rotationAxis, GLfloat rotation,
 	shapes_.push_back(
 		new Shape(
 			Shapelist::Tetrahedre,
-			GRIS_CLAIR_POUR_TEXTURES,
-			coords_,
+			glm::vec4(0.0, 0.0, 1.0, 1.0),
+			glm::vec3(0, 0, 0),
 			rotationAxis_,
-			rotation_,
-			scale_ * (GLfloat) 0.5
+			(GLfloat)0,
+			(GLfloat) 0.5
 		)
 	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Cube,
+			glm::vec4(0.1, 0.1, 0.1, 1.0),
+			glm::vec3(0., .2, -0.3),
+			glm::vec3(0.5, -0.3, 0.),
+			(GLfloat)-45,
+			(GLfloat) 0.8,
+			glm::vec3(0.5, 0.5, 0.05)
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Cube,
+			glm::vec4(0.1, 0.1, 0.1, 1.0),
+			glm::vec3(0., -.2, -0.3),
+			glm::vec3(0.5, 0.3, 0.),
+			(GLfloat)45,
+			(GLfloat) 0.8,
+			glm::vec3(0.5, 0.5, 0.05)
+		)
+	);
+
 
 }
