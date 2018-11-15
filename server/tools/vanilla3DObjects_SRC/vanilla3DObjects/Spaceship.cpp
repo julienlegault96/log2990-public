beef9258031a9e80a3ce499b9c82f536b9e7a5f8
Julien Legault <julien-3.legault@polymtl.ca>
2018-11-15 02:18:42 -0500

    ajout daile au spaceship

--- server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Spaceship.cpp
+++ server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Spaceship.cpp
@@ -4,16 +4,16 @@
 Spaceship::Spaceship(glm::vec3 coords, glm::vec3 rotationAxis, GLfloat rotation, GLfloat scale)
 	: CompositeShape(coords, rotationAxis, rotation, scale)
 {
-		shapes_.push_back(
-			new Shape(
-				Shapelist::Theiere,
-				glm::vec4(0.4, 0.4, 0.4, 1.0),
-				glm::vec3(0, 0, 0),
-				rotationAxis_,
-				(GLfloat)0,
-				(GLfloat) 0.1
-			)
-		);
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Theiere,
+			glm::vec4(0.4, 0.4, 0.4, 1.0),
+			glm::vec3(0, 0, 0),
+			rotationAxis_,
+			(GLfloat)0,
+			(GLfloat) 0.1
+		)
+	);
 
 	shapes_.push_back(
 		new Shape(
