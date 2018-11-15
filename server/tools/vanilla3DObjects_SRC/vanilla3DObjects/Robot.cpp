b68d181b49989d9aa0efc5ea21d4fa404929265c
Julien Legault <julien-3.legault@polymtl.ca>
2018-11-15 18:00:50 -0500

    code pour le robot fini

--- server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Robot.cpp
+++ server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Robot.cpp
@@ -4,10 +4,12 @@
 Robot::Robot(glm::vec3 coords, glm::vec3 rotationAxis, GLfloat rotation, GLfloat scale) :CompositeShape(coords, rotationAxis, rotation, scale) {
 
 	glm::vec4 colorBody = glm::vec4(0.4, 0.4, 0.4, 1);
-	glm::vec4 colorHead = glm::vec4(1.0, 0.5, 0.0, 0.0);
+	glm::vec4 colorHead = glm::vec4(1.0, 0., 0.0, 1.);
+    glm::vec4 colorFire = glm::vec4(0.5, 0.5, 0.2, 0.3);
+	glm::vec4 colorReactor = glm::vec4(0.2, 0.5, 0.2, 1);
 	glm::vec3 deformationLeg = glm::vec3(.4, .4, 1.2);
-	glm::vec3 deformationBody = glm::vec3(1., 2., 1.);
-	//glm::vec3 deformationArm = glm::vec3(.5, 0.6, 0.6);
+	glm::vec3 deformationBody = glm::vec3(.50, .70, .35);
+	glm::vec3 deformationArm = glm::vec3(.15, 0.4, 0.15);
 	glm::vec3 deformationH = glm::vec3(0.6, 0.6, 0.6);
 	glm::vec4 colorCyl = glm::vec4(0.0, 0.4, 0.3, 1);
 
@@ -22,6 +24,61 @@ Robot::Robot(glm::vec3 coords, glm::vec3 rotationAxis, GLfloat rotation, GLfloat
 			DEFAULT_ROTATION_AXIS,
 			DEFAULT_ROTATION,
 			deformationBody
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Cube,
+			colorHead,
+			glm::vec3(0,.3,0),
+			DEFAULT_ROTATION_AXIS,
+			DEFAULT_ROTATION,
+			0.25
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Cube,
+			colorHead,
+			glm::vec3(0.20, 0, 0),
+			DEFAULT_ROTATION_AXIS,
+			DEFAULT_ROTATION,
+			deformationArm
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Cube,
+			colorHead,
+			glm::vec3(-0.20, 0, 0),
+			DEFAULT_ROTATION_AXIS,
+			DEFAULT_ROTATION,
+			deformationArm
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::Cone,
+			colorReactor,
+			glm::vec3(0.0, -.35, 0.0),
+			glm::vec3(1, 0, 0),
+			270,
+			DEFAULT_SCALE * 0.4
+		)
+	);
+
+	shapes_.push_back(
+		new Shape(
+			Shapelist::ConeTronque,
+			colorFire,
+			glm::vec3(0.0, -0.35, 0.0),
+			glm::vec3(1.0, 0.0, 0.0),
+			90,
+			glm::vec3(0.2, 0.2, 0.8)
 		)
 	);
 }
