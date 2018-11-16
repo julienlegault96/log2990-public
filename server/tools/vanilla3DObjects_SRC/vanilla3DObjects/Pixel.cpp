07432cada6b2f8cfb6425b05e4adbb967c7e9ccf
Louis Sérey Larose <papalarose@yahoo.ca>
2018-11-16 10:28:36 -0500

    update des fichiers commons utilisés dans le genmulti
    LSLarose

--- server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Pixel.cpp
+++ server/tools/vanilla3DObjects_SRC/vanilla3DObjects/Pixel.cpp
@@ -1,19 +1,19 @@
 #include "Pixel.h"
 
-Pixel::Pixel() : r(255), g(255), b(255)
+Pixel::Pixel() : _red(255), _green(255), _blue(255)
 {
 }
 
-Pixel::Pixel(const unsigned char & r, const unsigned char & g, const unsigned & b) : r(r), g(g), b(b)
+Pixel::Pixel(const unsigned char & r, const unsigned char & g, const unsigned & b) : _red(r), _green(g), _blue(b)
 {
 }
 
 const bool Pixel::operator==(const Pixel & pixel) const
 {
-	return
-		this->r == pixel.r
-		&& this->g == pixel.g
-		&& this->b == pixel.b;
+	return 
+		this->_red == pixel._red
+		&& this->_green == pixel._green
+		&& this->_blue == pixel._blue;
 }
 
 const bool Pixel::operator!=(const Pixel & pixel) const
