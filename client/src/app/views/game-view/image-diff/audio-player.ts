700c01f674ac4abd3e0eef71b788c07a4caa47a1
Nourou-Dine Ahohoue <dineaho@gmail.com>
2018-11-16 17:21:34 -0500

    Identification des erreurs, bip sonore pour une identification erronnée. ND

--- client/src/app/views/game-view/image-diff/audio-player.ts
+++ client/src/app/views/game-view/image-diff/audio-player.ts
@@ -1,11 +1,17 @@
 export class AudioPlayer {
 
     private audio: HTMLAudioElement;
+    private failAudio: HTMLAudioElement;
 
     public constructor(source: string) {
         this.audio = new Audio();
         this.audio.src = source;
         this.audio.load();
+
+        // failAudio
+        this.failAudio = new Audio();
+        this.failAudio.src = source;
+        this.failAudio.load();
     }
 
     public play(): void {
