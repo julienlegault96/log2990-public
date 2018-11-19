68e90559b0fad46548ef186243f0bf593e4091c2
Nourou-Dine Ahohoue <dineaho@gmail.com>
2018-11-19 17:16:19 -0500

    Ajout d'une directive en vue d'ajouter le mot erreur à l'endroit du clic. ND

---
+++ client/src/app/views/game-view/solo-game/solo-game.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SoloGameDirective } from './solo-game.directive';
+
+describe('SoloGameDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SoloGameDirective();
+    expect(directive).toBeTruthy();
+  });
+});
