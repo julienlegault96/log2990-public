abf9fe04f9824e355a43a4436ddc63930ff1f5e3
Nourou-Dine Ahohoue <dineaho@gmail.com>
2018-11-20 16:51:44 -0500

    Implémentation de l'affichage du mot erreur à l'endroit d'un clic erronné en cours, ND

---
+++ client/src/app/views/game-view/game-error.directive.spec.ts
@@ -0,0 +1,8 @@
+import { GameErrorDirective } from "./game-error.directive";
+
+describe("GameErrorDirective", () => {
+  it("should create an instance", () => {
+    const directive = new GameErrorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
