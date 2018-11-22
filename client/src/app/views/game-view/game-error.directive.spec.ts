cef90f3fef99628527eac971e26afe1fded8ca18
Nourou-Dine Ahohoue <dineaho@gmail.com>
2018-11-22 16:01:44 -0500

    Affichage du mot erreur à l'endroit du clic. ND

--- client/src/app/views/game-view/game-error.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GameErrorDirective } from "./game-error.directive";
-
-describe("GameErrorDirective", () => {
-  it("should create an instance", () => {
-    const directive = new GameErrorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
