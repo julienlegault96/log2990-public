cbe29348f4893f7575a08693b21e4c41353305b8
Kevin Pastor <kevin.pastor@polymtl.ca>
2018-11-23 10:45:31 -0500

    Ordered imports. KP

--- client/src/app/app-routing.module.spec.ts
+++ client/src/app/app-routing.module.spec.ts
@@ -1,7 +1,8 @@
-import { AppRoutingModule } from "./app-routing.module";
-import { UserService } from "./services/user/user.service";
 import { TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { UserService } from "./services/user/user.service";
 
 describe("AppRoutingModule", () => {
     let appRoutingModule: AppRoutingModule;
