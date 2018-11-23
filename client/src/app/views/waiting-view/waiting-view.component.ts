d5837b03a85f3f7fd8c4c30286a35cc42c1886e4
julienlegault96 <julien-3.legault@polymtl.ca>
2018-11-23 16:01:51 -0500

    ajout dun boutton pour quitter la page d attent JL & SBe

--- client/src/app/views/waiting-view/waiting-view.component.ts
+++ client/src/app/views/waiting-view/waiting-view.component.ts
@@ -1,8 +1,17 @@
 import { Component } from "@angular/core";
+
+import { Router } from "@angular/router";
 @Component({
     selector: "app-waiting-view",
     templateUrl: "./waiting-view.component.html"
 })
 
 export class WaitingViewComponent {
+    public constructor(
+        private router: Router
+    ) {}
+
+    public cancelGameCreation(): void {
+        this.router.navigate(["/", "gameList"]);
+    }
 }
