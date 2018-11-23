f6c18186ad83f16db86447ba58a81b6a3e6abc2a
julienlegault96 <julien-3.legault@polymtl.ca>
2018-11-23 13:53:32 -0500

    ajout du component waititng view JL & SB

---
+++ client/src/app/views/waiting-view/waiting-view.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: "app-waiting-view",
+    templateUrl: "./waiting-view.component.html"
+})
+
+export class WaitingViewComponent {
+}
