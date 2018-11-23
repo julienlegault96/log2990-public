bb99d846d14f892af8176f0ed2ac0efc32a53b74
Nourou-Dine Ahohoue <dineaho@gmail.comm>
2018-11-23 16:47:58 -0500

    Affichage du mot erreur ^l'endroit du clic, OK. ND, KP

--- client/src/app/views/game-view/image-diff/audio-player.ts
+++ client/src/app/views/game-view/image-diff/audio-player.ts
@@ -1,27 +1,21 @@
 export class AudioPlayer {
 
     private audio: HTMLAudioElement;
+    private failAudio: HTMLAudioElement;
 
     public constructor(source: string) {
         this.audio = new Audio();
         this.audio.src = source;
         this.audio.load();
+
+        // failAudio
+        this.failAudio = new Audio();
+        this.failAudio.src = source;
+        this.failAudio.load();
     }
 
     public play(): void {
         const clone: HTMLAudioElement = this.audio.cloneNode() as HTMLAudioElement;
         clone.play();
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-    public playFailAudio(): void {
-        const clone: HTMLAudioElement = this.failAudio.cloneNode() as HTMLAudioElement;
-        clone.play();    // A modifier.
-    }
-
->>>>>>> Identification des erreurs, bip sonore pour une identification erronnée. ND
-=======
->>>>>>> Ajout de sonnerie pour erreur d'identification, ND
 }
