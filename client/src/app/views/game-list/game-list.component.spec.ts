b612bf5b0d8337244bc56e5f527fcd4627c85e47
Louis Sérey Larose <papalarose@yahoo.ca>
2018-11-23 16:23:01 -0500

    fix des providers de certains tests, modernisation de ceux-ci
    LSLarose

--- client/src/app/views/game-list/game-list.component.spec.ts
+++ client/src/app/views/game-list/game-list.component.spec.ts
@@ -1,17 +1,19 @@
 import { TestBed, ComponentFixture, fakeAsync, tick } from "@angular/core/testing";
 import { HttpClientModule, HttpClient } from "@angular/common/http";
-
+import { RouterTestingModule } from "@angular/router/testing";
 import { TestHelper } from "../../../test.helper";
-import { GameListComponent } from "./game-list.component";
-import { GameCardComponent } from "./game-card/game-card.component";
-import { LeaderboardComponent } from "./leaderboard/leaderboard.component";
-import { GameService } from "../../services/game/game.service";
 
 import { GameType } from "../../../../../common/game/game-type";
 import { GAMES, SINGLE_VIEW_GAME_COUNT, DOUBLE_VIEW_GAME_COUNT } from "../../../../../common/game/mock-games";
-import { RouterTestingModule } from "@angular/router/testing";
+
+import { GameListComponent } from "./game-list.component";
+import { GameCardComponent } from "./game-card/game-card.component";
+import { LeaderboardComponent } from "./leaderboard/leaderboard.component";
+
+import { GameService } from "../../services/game/game.service";
 import { SocketService } from "src/app/services/socket/socket.service";
 import { UserService } from "src/app/services/user/user.service";
+import { MessageService } from "src/app/services/message/message.service";
 
 describe("GameListComponent", () => {
     let component: GameListComponent;
@@ -40,6 +42,7 @@ describe("GameListComponent", () => {
                 { provide: GameService, useValue: gameService },
                 { provide: SocketService, useValue: socketServiceSpy },
                 { provide: UserService, useValue: userServiceSpy },
+                MessageService
             ],
             imports: [
                 HttpClientModule,
