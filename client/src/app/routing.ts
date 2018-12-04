a0e60bff43d9a05341ef9e8541a25e0e02d96885
KevinPastor <kevin.pastor@polymtl.ca>
2018-12-03 20:33:20 -0500

    Added enum file. KP

---
+++ client/src/app/routing.ts
@@ -0,0 +1,12 @@
+export enum Routing {
+    Homepage = "",
+    Admin = "admin",
+    GameList = "gameList",
+    Game = "game",
+    Waiting = "waiting",
+}
+
+export enum RoutingGameMatchId {
+    Solo = "solo",
+    Duel = "duel",
+}
